7befe136c76180cb3b8ad2ee27bdde4d0e2f3bae
Michał Kaźmierczak <michal.kazmierczak@zielonakamera.pl>
2022-09-27 16:56:43 +0200

    feed

--- src/getFeed.php
+++ src/getFeed.php
@@ -5,7 +5,7 @@ try {
   $file = file_get_contents("https://preview.wgrygranie.pl/js/instatest.json");
   $data = json_decode($file);
   $graphQL = $data->graphql->user->edge_owner_to_timeline_media->edges;
-  foreach ($graphQL as $edge) |{
+  foreach ($graphQL as $edge) {
       if ($edge->node->__typename == "GraphImage") {
           $shortCode = $edge->node->shortcode;
           $displayURL = $edge->node->display_url;
