d61a05902bff5644893667b4659bb72972398fd4
Michał Kaźmierczak <michal.kazmierczak@zielonakamera.pl>
2022-09-27 16:44:57 +0200

    feed

--- src/getFeed.php
+++ src/getFeed.php
@@ -2,5 +2,5 @@
 
 $file = file_get_contents("https://preview.wgrygranie.pl/js/instatest.json");
 $data = json_decode($file);
-$graphQL = $data?->graphql?->user?->edge_owner_to_timeline_media;
+$graphQL = $data->graphql->user->edge_owner_to_timeline_media;
 print_r ($graphQL);
