f330862b53f4012acaced42a48f3722a28a58a91
Michal Kazmierczak <michal.kazmierczak@oldwestenterprises.pl>
2022-09-28 16:35:55 +0200

    corrected mail text

--- src/Controllers/ContactFormController.php
+++ src/Controllers/ContactFormController.php
@@ -38,7 +38,7 @@ class ContactFormController extends Controller {
   private function sendMail($name, $surname, $subject, $message, $email) {
     $mail_address = 'sklep@wgrygranie.pl';
     $to      = $mail_address;
-    $message .= "From: ".$name." ".$surname." (".$email.")"."\r\n".$message;
+    $message = "From: ".$name." ".$surname." (".$email.")"."\r\n\r\n".$message;
     $headers = 'From: '.$mail_address."\r\n" .
       'Reply-To: '.$email. "\r\n" .
       'X-Mailer: PHP/' . phpversion();
